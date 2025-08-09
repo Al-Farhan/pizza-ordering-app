b5e3ab071c24ffffc7d2f90e31e3acfa3ffc8437
Farhan Shaikh <programmingwithfarhan@gmail.com>
2025-08-10 00:20:38 +0530

    Expo Router Screens

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text } from "react-native";
 import Colors from "@/src/constants/Colors";
 import { Product } from "@/src/types";
+import { Link } from "expo-router";
 
 export const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
@@ -11,17 +12,19 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: product.image || defaultPizzaImage,
-        }}
-        style={styles.image}
-        resizeMode="contain" // contain the image to its bg size
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{
+            uri: product.image || defaultPizzaImage,
+          }}
+          style={styles.image}
+          resizeMode="contain" // contain the image to its bg size
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
