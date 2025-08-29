6a76f41853289bd376c852c96ba1c6a35eba437a
Farhan Shaikh <programmingwithfarhan@gmail.com>
2025-08-29 23:48:51 +0530

    Top Tab Navigator

---
+++ src/app/(admin)/orders/list/index.tsx
@@ -0,0 +1,19 @@
+import { FlatList, StyleSheet } from "react-native";
+
+import { Text, View } from "@/src/components/Themed";
+import orders from "@/assets/data/orders";
+import OrderListItem from "@/src/components/OrderListItem";
+import { Stack } from "expo-router";
+
+export default function OrdersScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Orders" }} />
+      <FlatList
+        data={orders}
+        renderItem={({ item }) => <OrderListItem order={item} />}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+      />
+    </>
+  );
+}
